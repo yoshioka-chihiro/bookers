d172a1a8c5a2cba2ff18b8ddcd89b78d2a8c5686
ChihiroYoshioka <yohokaaaa@gmail.com>
2022-04-05 10:52:11 +0000

    [Update]edit.html.erb

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,12 +19,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   private
-  
+
   def book_params
    params.require(:book).permit(:title, :body)
   end
-  
+
 end
