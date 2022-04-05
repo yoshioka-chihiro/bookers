75b90730f190dc73786a1596b329879a8d030188
ChihiroYoshioka <yohokaaaa@gmail.com>
2022-04-05 07:37:40 +0000

    [Update]books_controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,15 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+ def create
+   book = Book.new(book_params)
+   book.save
+   redirect_to '/'
+ end
+
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -12,4 +19,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def book_params
+   params.require(:book).permit(:title, :body)
+  end
+  
 end
